4ff9072853bc1cd8434883610cdd364098d429cc
Aldo <rivala647@gmail.com>
2025-05-20 20:48:15 -0600

    v4

--- src/main/java/com/taskmanager/dts/config/WebMvcConfig.java
+++ src/main/java/com/taskmanager/dts/config/WebMvcConfig.java
@@ -2,17 +2,20 @@ package com.taskmanager.dts.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// Esta clase debe eliminarse o comentarse ya que duplica la funcionalidad de MvcConfig
-/*
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/");
     }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
 }
-*/
